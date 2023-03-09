0c7a31d238448ea6509fd735ab8c7fd49940f24e
travisdjones78 <travisdjones2000@gmail.com>
2023-03-08 19:46:00 -0500

    creating a new branch for next lesson (#7)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-const todoList = [
-  {
-    id: '1',
-    title: 'Record Music'
-  },
-  {
-    id: '2',
-    title: 'Write Code'
-  },
-  {
-    id: '3',
-    title: 'Connect Wires'
-  }
-]
+import { React } from "react";
+import ToDoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>ToDo List</h1>
-      <ul>
-        {todoList.map(item => {
-          return <li key={item.id}>
-            <span>{item.title}</span>
-          </li>
-        })}
-      </ul>
+      {AddTodoForm()}
+      {ToDoList()}
     </div>
   );
 }
