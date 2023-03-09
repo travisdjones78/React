2123974353a2811f0f709d0bfe72d8262fe0d22f
travisdjones78 <travisdjones2000@gmail.com>
2023-03-09 02:24:39 -0500

    updated as new branch for lesson 4 (#9)
    * creating a new branch for next lesson
    
    * updated as new branch for lesson
    
    * updated as new branch for lesson 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { React } from "react";
+import { React, useState } from "react";
 import ToDoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
+  const [todoList, setTodoList] = useState([])
+  const addTodo = (newTodo)=>{
+    setTodoList([...todoList,newTodo])
+  }
+
   return (
     <div>
       <h1>ToDo List</h1>
-      {AddTodoForm()}
-      {ToDoList()}
+
+      <AddTodoForm onAddTodo={addTodo} />
+      <ToDoList todoList={todoList}/>
+
     </div>
   );
 }
