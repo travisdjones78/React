b2d5a831631b1e8013b0e880be412ccf0975639e
travisdjones78 <travisdjones2000@gmail.com>
2023-03-09 02:06:23 -0500

    Lesson1-3(#8)
    * creating a new branch for next lesson
    
    * updated as new branch for lesson

---
+++ src/TodoListItem.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+function TodoListItem() {
+    return (
+        <li key={item.id}>
+            <span>{item.title}</span>
+        </li>
+    )
+}
+
+export default TodoListItem;
